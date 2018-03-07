9efa6b58d359c2ea31438160a6de93927da6aa3b
Owner <Owner@192.168.4.107>
2018-03-07 11:18:09 +0900

    설저어엉

---
+++ kkst_project/src/main/java/com/pro/kkst/daos/KkstResDao.java
@@ -0,0 +1,11 @@
+package com.pro.kkst.daos;
+
+import java.util.List;
+
+import com.pro.kkst.dtos.ResDto;
+
+public interface KkstResDao {
+	
+	public List<ResDto> ResList(String cate);
+
+}
---
+++ kkst_project/src/main/java/com/pro/kkst/service/KkstResService.java
@@ -0,0 +1,9 @@
+package com.pro.kkst.service;
+
+import java.util.List;
+
+import com.pro.kkst.dtos.ResDto;
+
+public interface KkstResService {
+	public List<ResDto> ResList(String cate);
+}
